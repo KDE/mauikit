ef9c6e5cfb23dea673a9c39d52bf70e687f0da1c
Camilo Higuita <chiguitar@unal.edu.co>
2022-11-10 11:57:45 -0500

    go back to using ki18n and fix mistakes

--- Messages.sh
+++ Messages.sh
@@ -6,4 +6,4 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 
-$XGETTEXT $(find src/ -name \*.cpp -o -name \*.h -o -name \*.qml) -o $podir/MauiKitPlugin.pot
+$XGETTEXT $(find src/ -name \*.cpp -o -name \*.h -o -name \*.qml) -o $podir/mauikit.pot
