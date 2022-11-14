700ab2ac4fceaa7f5bdc77443a27152bd2f5aa00
Camilo Higuita <chiguitar@unal.edu.co>
2022-11-14 10:17:30 -0500

    add line for missing translations on the style

--- Messages.sh
+++ Messages.sh
@@ -6,4 +6,4 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 
-$XGETTEXT $(find src/ -name \*.cpp -o -name \*.h -o -name \*.qml) -o $podir/mauikit.pot
+$XGETTEXT $(find . -name \*.cpp -o -name \*.h -o -name \*.qml) -o $podir/mauikit.pot
